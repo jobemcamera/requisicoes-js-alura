32ed44fbc055a0c76439671dd8141c46290ecbf8
jobemcamera <jobecamera@gmail.com>
2023-01-30 18:25:12 -0300

    função async criaVideo com method POST para receber videos novos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,27 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    // padrão da requisição é GET. Necessário mudar com method: POST
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos, 
+    criaVideo
 }
